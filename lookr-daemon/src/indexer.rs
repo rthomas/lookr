87b6f82547a8e1fea7a4cb3ea343e9872aa45431
Ryan Thomas <ryan@ryant.org>
2020-05-30 09:40:47 +1000

    Fix up the indexer so that we don't commit after every change.
    This sets both a timeout and a mutation threshold for the index commit
    (5s and 1000 respecitvely) to make sure we're not commiting after
    every single change, which would slow things right down on a large
    number of change events.
    
    There may be a better solution to this with recv_deadline, however
    that is still in nightly.

--- lookr-daemon/src/indexer.rs
+++ lookr-daemon/src/indexer.rs
@@ -5,7 +5,7 @@ use std::error;
 use std::fmt;
 use std::io;
 use std::path::{Path, PathBuf};
-use std::sync::mpsc::{channel, RecvError, Sender};
+use std::sync::mpsc::{channel, RecvError, RecvTimeoutError, Sender};
 use std::thread;
 use std::time::{Duration, Instant};
 use tantivy::schema::{Schema, STORED, TEXT};
@@ -62,7 +62,7 @@ impl<'a> Indexer<'a> {
         let field_ext = self.schema.get_field(FIELD_EXT).unwrap();
         let field_filename = self.schema.get_field(FIELD_FILENAME).unwrap();
 
-        let index_pathbuf = |p: &PathBuf| {
+        let from_pathbuf = |p: &PathBuf| {
             let mut doc = Document::new();
             doc.add_text(field_path, &p.to_string_lossy());
             match p.extension() {
@@ -88,7 +88,7 @@ impl<'a> Indexer<'a> {
                     Ok(e) => {
                         let p = e.into_path();
                         debug!("Indexing: {:?}", p);
-                        index_writer.add_document(index_pathbuf(&p));
+                        index_writer.add_document(from_pathbuf(&p));
                     }
                     Err(e) => {
                         error!("Walkdir Error: {}", e);
@@ -107,41 +107,58 @@ impl<'a> Indexer<'a> {
 
         info!("Indexer watching for change events...");
         // Wait for watcher events and index those.
+        let mut counter: u32 = 1;
+        let mut last_change = counter;
         loop {
-            match rx.recv() {
+            // This will increment the counter and commit if we have processed
+            // a number of documents (1000). This is to prevent us never
+            // getting to the commit timeout if we are constantly churning
+            // events.
+            if counter % 1000 == 0 {
+                info!("Commiting index after 1000 mutations.");
+                match index_writer.commit() {
+                    Ok(_) => (),
+                    Err(e) => error!("Could not commit IndexWriter: {}", e),
+                };
+            }
+
+            match rx.recv_timeout(Duration::from_secs(1)) {
                 Ok(WatchEvent::Create(pb)) => {
-                    // TODO: Find a way to generalize the conversion from PathBuf to Document
                     debug!("CREATE: {:?}", pb);
-                    index_writer.add_document(index_pathbuf(&pb));
-                    match index_writer.commit() {
-                        Ok(_) => (),
-                        Err(e) => error!("Could not commit IndexWriter: {}", e),
-                    }
+                    index_writer.add_document(from_pathbuf(&pb));
+                    counter += 1;
                 }
                 Ok(WatchEvent::Remove(pb)) => {
-                    info!("REMOVE: {:?}", pb);
+                    debug!("REMOVE: {:?}", pb);
                     // TODO: This is not working - the document is still in the index...
                     let term = Term::from_field_text(field_path, &pb.to_string_lossy());
                     index_writer.delete_term(term);
-                    match index_writer.commit() {
-                        Ok(_) => (),
-                        Err(e) => error!("Could not commit IndexWriter: {}", e),
-                    }
+                    counter += 1;
                 }
                 Ok(WatchEvent::Rename(pb_src, pb_dst)) => {
                     debug!("RENAME: {:?} -> {:?}", pb_src, pb_dst);
                     let term = Term::from_field_text(field_path, &pb_src.to_string_lossy());
                     index_writer.delete_term(term);
-                    index_writer.add_document(index_pathbuf(&pb_dst));
-                    match index_writer.commit() {
-                        Ok(_) => (),
-                        Err(e) => error!("Could not commit IndexWriter: {}", e),
+                    index_writer.add_document(from_pathbuf(&pb_dst));
+                    counter += 1;
+                }
+                Err(e) => match e {
+                    RecvTimeoutError::Timeout => {
+                        // Don't keep commiting if we're just idle.
+                        if last_change != counter {
+                            debug!("Commiting index after receiver timeout");
+                            last_change = counter;
+                            match index_writer.commit() {
+                                Ok(_) => (),
+                                Err(e) => error!("Could not commit IndexWriter: {}", e),
+                            }
+                        }
                     }
-                }
-                Err(e) => {
-                    error!("Error from the RX channel for the FsWatcher: {}", e);
-                    return Err(IndexerError::WatcherRxError(e));
-                }
+                    RecvTimeoutError::Disconnected => {
+                        error!("Error from the RX channel for the FsWatcher: {}", e);
+                        return Err(IndexerError::WatcherRxError(e));
+                    }
+                },
             }
         }
     }
@@ -157,7 +174,7 @@ impl Drop for Indexer<'_> {
 pub enum IndexerError {
     IoError(io::Error),
     Tantivy(TantivyError),
-    WatcherRxError(RecvError),
+    WatcherRxError(RecvTimeoutError),
     Watcher(WatcherError),
 }
 
@@ -299,5 +316,46 @@ mod test {
                 c => println!("OTHER: {:?}", c),
             }
         }
+    }
+
+    #[test]
+    fn test_tantivy() {
+        use tantivy::collector::TopDocs;
+        use tantivy::query::QueryParser;
+        use tantivy::schema::*;
+        use tantivy::{doc, Index};
+
+        let mut schema_builder = Schema::builder();
+        let title = schema_builder.add_text_field("title", TEXT | STORED);
+        let schema = schema_builder.build();
+
+        let index = Index::create_in_ram(schema.clone());
+
+        let title_str = "The modern Promotheus";
+        let query_str = "Promotheus";
+
+        let mut index_writer = index.writer_with_num_threads(1, 50_000_000).unwrap();
+        index_writer.add_document(doc!(title => title_str));
+        index_writer.commit().unwrap();
+
+        let searcher = index.reader().unwrap().searcher();
+        let query_parser = QueryParser::for_index(&index, vec![title]);
+        let query_promo = query_parser.parse_query(query_str).unwrap();
+        let top_docs_promo = searcher
+            .search(&query_promo, &TopDocs::with_limit(1))
+            .unwrap();
+
+        assert_eq!(top_docs_promo.len(), 1);
+
+        let term = Term::from_field_text(title, title_str);
+        index_writer.delete_term(term);
+        index_writer.commit().unwrap();
+
+        let query_promo2 = query_parser.parse_query(query_str).unwrap();
+        let top_docs_promo2 = searcher
+            .search(&query_promo2, &TopDocs::with_limit(1))
+            .unwrap();
+
+        assert_eq!(top_docs_promo2.len(), 0);
     }
 }
